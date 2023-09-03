b174e4deea054908602cd3c41d96bf5ac40ad1cf
Rishab789 <beraprakash456@gmail.com>
2023-09-03 10:25:32 +0530

    commited now

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -27,8 +27,8 @@ const Portfolio = () => {
        
     ];
   return (
-    <div name="portfolio" className="min-w-fit text-start bg-gradient-to-b from-black to-gray-800 h-full text-white md:h-screen ">
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+    <div name="portfolio" className=" w-full text-start bg-gradient-to-b from-black to-gray-800 h-full text-white md:h-screen ">
+        <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
 
             <div className="pb-8"> 
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
